b61e2cc388ce7d08be768e42469090660c262c74
s h <hirayama312@gmail.com>
2021-03-06 07:46:25 +0900

    usersモデル作成

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Users
+
+
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    pass
--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    age = models.IntegerField(null=True)
+    mail_address = models.EmailField()
+    created_add = models.DateTimeField(auto_now_add=True)
+    update_add = models.DateTimeField(auto_now=True)
