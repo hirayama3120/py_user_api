dc101b7f391b063b115c843755e878e52d8e2f98
s h <hirayama312@gmail.com>
2021-04-30 00:59:03 +0900

    get(all), post実装

--- user/serializer.py
+++ user/serializer.py
@@ -2,15 +2,29 @@ from rest_framework import serializers
 
 from .models import Users
 
-
 class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
-        fields = (
-            'first_name', 
+        fields = [
+            'first_name',
             'last_name',
             'age',
-            'mail_address',
-            'created_add',
-            'updated_add'
-            )
+            'mail_address'
+        ]
+        extra_kwargs = {
+            'first_name': {
+                'required': True,
+                'allow_blank': False,
+                'max_length': 32,
+            },
+            'last_name': {
+                'required': True,
+                'allow_blank': False,
+                'max_length': 32,
+            },
+            'mail_address': {
+                'required': True,
+                'allow_blank': False,
+            },
+        }
+
--- user/views.py
+++ user/views.py
@@ -1,9 +1,24 @@
 import django_filters
-from rest_framework import viewsets, filters
+
+from rest_framework import status, views
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
 from .models import Users
 from .serializer import UsersSerializer
 
-class UsersViewSet(viewsets.ModelViewSet):
-    queryset = Users.objects.all()
-    serializer_class = UsersSerializer
+class UserListCreateAPIView(views.APIView):
+    def get(self, request, *args, **kwargs):
+        users = Users.objects.all()
+        serializer = UsersSerializer(instance=users, many=True)
+        if not serializer.is_valid:
+            raise ValidationError(serializer.errors)
+
+        return Response(serializer.data, status.HTTP_200_OK)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = UsersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
